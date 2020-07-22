09f9742644e7188849d62351136e0c9f3497fdd7
Cursos-aap <eduardo.jimenez.mir@gmail.com>
2020-07-22 12:15:38 -0500

    More

--- src/exercises/Exercises.java
+++ src/exercises/Exercises.java
@@ -1,5 +1,6 @@
 package exercises;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercises {
@@ -83,8 +84,58 @@ public class Exercises {
         return vector_r;
     }
 
-    public static void sumMatrix(){
+    public static void sumMatrix(double[][] matrix_1, double[][] matrix_2){
 
+        if(matrix_1.length != matrix_2.length){
+            System.out.println("Las matrices no coinciden en # filas");
+            return;
+        }
+
+        short columns = (short) matrix_1[0].length;
+
+        for (int i = 0; i < matrix_2.length; i++) {
+            if(matrix_1[i].length != matrix_2[i].length || matrix_2[i].length != columns ){
+                System.out.println("Las matrices no coinciden en # columnas");
+                return;
+            }
+        }
+
+        double[][] matrix_r = new double[matrix_1.length][matrix_1[0].length];
+
+        for (int i = 0; i < matrix_2.length; i++)
+            for (int j = 0; j < matrix_1[0].length; j++)
+                matrix_r[i][j] = matrix_2[i][j] + matrix_1[i][j];
+
+        System.out.println(Arrays.deepToString(matrix_r));
+    }
+
+    public static long sumWithGauss(final int n, final int m){
+        //     condición   +   ? instrucciones en caso verdadero +      : instrucciones en caso falso
+        return n >= 0 && m > n ? ( ((long) m + 1) * m - (n - 1) * n) /2 : 0;
+        // m > 0
+        // m > n >= 0
+    }
+
+    public static void printTriangle(byte height){
+        StringBuilder str_builder = new StringBuilder();
+        for (byte i = 0; i < height; i++) {
+            str_builder.append(" ".repeat(height - i - 1));
+            str_builder.append("*".repeat(i * 2 + 1));
+            str_builder.append('\n');
+        }
+        System.out.println(str_builder);
+    }
+    
+    public static void printTriangle2(byte height){
+        StringBuilder str_builder = new StringBuilder();
+        for (byte i = 0; i < height; i++) {
+            for (int j = 0; j < height -i -1; j++)
+                str_builder.append(' ');
+            for (int j = 0; j < i * 2 +1; j++)
+                str_builder.append('*');
+            str_builder.append('\n');
+        }
+        System.out.println(str_builder);
     }
 
 }
